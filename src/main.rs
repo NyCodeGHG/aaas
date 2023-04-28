297951f2059c966d9bd47632fe3a1bfdd459ad28
Marie Ramlow <me@nycode.dev>
2023-04-28 20:22:52 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use std::io::Cursor;
+
+use agg::Config;
+use anyhow::Result;
+use axum::{
+    body::Bytes,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::{get, post},
+    Router, Server,
+};
+use tokio::signal::unix::SignalKind;
+use tracing::info;
+
+macro_rules! signal {
+    ($signal:expr) => {
+        async {
+            let mut signal = ::tokio::signal::unix::signal($signal).unwrap();
+            signal.recv().await;
+        }
+    };
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+    let app = Router::new()
+        .route("/render", post(render))
+        .route("/", get(|| async { ":3" }));
+    let server = Server::bind(&"0.0.0.0:8080".parse().unwrap());
+    info!("Listening on http://localhost:8080");
+    server
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(async {
+            tokio::select! {
+                _ = signal!(SignalKind::interrupt()) => {
+                    info!("Received SIGINT. Shutting down.");
+                },
+                _ = signal!(SignalKind::terminate()) => {
+                    info!("Received SIGTERM. Shutting down.");
+                },
+            }
+        })
+        .await
+        .unwrap();
+}
+
+#[axum::debug_handler]
+async fn render(body: Bytes) -> Result<Response, StatusCode> {
+    let gif = tokio::task::spawn_blocking(|| {
+        let mut output = Vec::new();
+        let input = Cursor::new(body);
+        agg::run(input, &mut output, Config::default())?;
+        Result::<Vec<u8>>::Ok(output)
+    })
+    .await
+    .unwrap()
+    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    Ok((StatusCode::OK, gif).into_response())
+}
