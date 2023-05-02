7ac0a13e4d0b0b3034bc215a4b9f76090b84ec80
Marie Ramlow <me@nycode.dev>
2023-05-02 07:58:50 +0200

    add flake again

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Asciinema as a Service";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+        fontsConf = pkgs.makeFontsConf {
+          fontDirectories = [ pkgs.jetbrains-mono ];
+        };
+
+        craneLib = crane.lib.${system};
+        aaas = craneLib.buildPackage {
+          pname = "aaas";
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+        };
+        dockerImage = pkgs.dockerTools.buildImage {
+          name = "aaas";
+          config = {
+            Cmd = [ "${aaas}/bin/aaas" ];
+          };
+          runAsRoot = ''
+            mkdir -p /etc/fonts
+            ln -s ${fontsConf} /etc/fonts/fonts.conf
+          '';
+        };
+      in
+      {
+        defaultPackage = aaas;
+        packages = {
+          inherit aaas;
+          docker = dockerImage;
+        };
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
+          shellHook = ''
+            export CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
+          '';
+        };
+      });
+}
